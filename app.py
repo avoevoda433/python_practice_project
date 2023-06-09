4a3872e4a72bd5670a66eb6efa97f0988dfdf29c
Voevoda Alexey <avoevoda433@gmail.com>
2023-06-09 21:41:48 +0300

    Developed the html page layout.

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def create_app():
     app.config.from_object('config.DevelopmentConfig')
     app.register_blueprint(app_page)
 
-    docs_db = Database('docs_db')
-    docs_db.create_table('documents', (('Title', 'text'), ('Data', 'text')))
+    # docs_db = Database('docs_db')
+    # docs_db.create_table('documents', (('Title', 'text'), ('Data', 'text')))
     # docs_db.drop_table('documents')
-    docs_db.show_tables()
-    docs_db.insert_data('documents', ('Test_title_1', 'Some_information_about_document_1'))
-    docs_db.get_all_table_data('documents')
+    # docs_db.show_tables()
+    # docs_db.insert_data('documents', ('Test_title_1', 'Some_information_about_document_1'))
+    # docs_db.get_all_table_data('documents')
 
     return app
 
